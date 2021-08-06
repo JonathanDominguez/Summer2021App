1e6150a1223bcf6334196da3ee352cf5efda0df6
Jonathan Dominguez <DominguezE.Jonathan@gmail.com>
2021-08-05 18:22:33 -0700

    updated css download page

--- client/src/Components/download-version/DownloadVersion.js
+++ client/src/Components/download-version/DownloadVersion.js
@@ -2,57 +2,43 @@ import React from 'react'
 import './DownloadVersion.css'
 import resumeImage from '../../images/sample.png'
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-// require("downloadjs")(data, strFileName, strMimeType);
 import download from 'downloadjs'
 
 
 const DownloadVersion = (props) => {
     const { data } = props;
-    // const outPut = () => {
-    //     console.log(data)
-    // }
 
-    async function modifyPdf() {
-        console.log("ss")
-      // Fetch an existing PDF document
+    async function downlodPdf() {
         const url = 'https://pdf-lib.js.org/assets/with_update_sections.pdf';
         const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
-
-      // Load a PDFDocument from the existing PDF bytes
         const pdfDoc = await PDFDocument.load(existingPdfBytes);
-
-      // Embed the Helvetica font
         const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-      // Get the first page of the document
         const pages = pdfDoc.getPages();
         const firstPage = pages[0];
-
-      // Get the width and height of the first page
         const { width, height } = firstPage.getSize();
-
-      // Draw a string of text diagonally across the first page
         firstPage.drawText(data.firstName, {
         x: 5,
         y: height / 2 + 300,
         size: 50,
         font: helveticaFont,
         color: rgb(0.95, 0.1, 0.1),
-        rotate: degrees(-45),
+        rotate: degrees(0),
     })
-
-      // Serialize the PDFDocument to bytes (a Uint8Array)
         const pdfBytes = await pdfDoc.save();
+        download(pdfBytes, "Resume.pdf", "application/pdf");
+    }
 
-		// 	// Trigger the browser to download the PDF document
-        download(pdfBytes, "pdf-lib_modification_example.pdf", "application/pdf");
+    function downlodDoc (){
+        console.log('IN progress')
     }
 
     return (
         <div>
             <div className="main-container">
                 <div className="main-item">
-                    <p className = "heading-text">How would you like to export your work?</p>
+                    <div className = "text-box">
+                        <p className = "heading-text">How would you like to export your work?</p>
+                    </div>
                 </div>
                 <div className="main-item">
                     <div className="download-container">
@@ -62,7 +48,7 @@ const DownloadVersion = (props) => {
                                     <img src={resumeImage} className = "image-fix" alt ="resume"/>
                                 </div>
                                 <div className="image-item">
-                                    <button className = "button-type" onClick={modifyPdf}>PDF</button>
+                                    <button className = "button-type" onClick={downlodPdf}>PDF</button>
                                 </div> 
                             </div>
                         </div>
@@ -72,7 +58,7 @@ const DownloadVersion = (props) => {
                                     <img src={resumeImage} className = "image-fix" alt ="resume"/>
                                 </div>
                                 <div className="image-item">
-                                    <button className = "button-type">Microsoft Word</button>
+                                    <button className = "button-type" onClick={downlodDoc}>Microsoft Word</button>
                                 </div> 
                             </div>                                       
                         </div> 
